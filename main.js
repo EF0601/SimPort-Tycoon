4fe22eaee79fb6f69b018a78f35eaca9fc1c8ecb
Forever Coding <70299402+EF0601@users.noreply.github.com>
2023-06-19 21:35:34 -0400

    v0.1.0-alpha

---
+++ main.js
@@ -0,0 +1,21 @@
+let unlocked = {
+     taxi: true,
+     advancedTaxi: false,
+     bus: false,
+     advancedBus: false,
+}
+let day = 1;
+let month = 1;
+
+//day counter
+setInterval(() => {
+     if(day != 30){
+          day++;
+     }
+     else if(day >= 30){
+          day = 1;
+          month++;
+     }
+     document.querySelector('.day').textContent = day;
+     document.querySelector('.month').textContent = month;
+}, 2000);
