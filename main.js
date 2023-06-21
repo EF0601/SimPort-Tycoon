9ed55e6f7a790f008f78e1bd34e8956e1bf158bf
Forever Coding <70299402+EF0601@users.noreply.github.com>
2023-06-20 20:30:47 -0400

    v0.4.0
    Added unlocking
    Worked on buying of basic taxis ONLY.
    Income system complete.
    Research buttons' colors change depending on able to buy research.

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-let unlocked = {
-     taxi: true,
-     advancedTaxi: false,
-     bus: false,
-     advancedBus: false,
-}
 let day = 1;
 let month = 1;
+
+let money = 500;
+
+let speed = 1500;
+
+//assets values
+let assets = {
+     taxis: 0, //sets amount of taxis to make money
+     advancedTaxis: 0,
+     taxiPaxCapacity: 4, //amount of max passengers, limiting income
+     advancedTaxiPaxCapacity: 8,
+
+     buses: 0,
+     advancedBuses: 0,
+     busPaxCapacity: 20,
+     advancedBusPaxCapacity: 45,
+
+     //popularity, limiting passenger amount
+     taxiPopularity: -2,
+     busPopularity: -4,
+}
 
 //day counter
 setInterval(() => {
@@ -18,6 +33,115 @@ setInterval(() => {
      }
      document.querySelector('.day').textContent = day;
      document.querySelector('.month').textContent = month;
-}, 2000);
+     
+     document.querySelector('.money').textContent = money;
+     
+     taxi();
+     bus();
+     updateUnlockValidity();
+}, speed);
+
+//speed setting [FAILED]
+function setSpeed(target){
+     speed = target;
+}
 
 //unlocking
+let unlocked = {
+     taxi: true,
+     advancedTaxi: false,
+     bus: false,
+     advancedBus: false,
+}
+let unlockBoxes = {
+     advancedTaxi: document.querySelector('.advancedTaxiResearch'),
+     bus: document.querySelector('.basicBusResearch'),
+     advancedBus: document.querySelector('.advancedBusResearch'),
+}
+function unlock(thing){
+     switch (thing) {
+          case "advTaxi":
+               if (unlocked.advancedTaxi != true) {
+                if (month >= 2 && money >= 200) {
+                    money = money - 200;
+                    document.querySelector('.news').textContent = "SimPort, LLC just unlocked Advanced Taxis! Onlookers look forward to higher income."
+                }
+               }
+               break;
+               case "bus":
+               if (unlocked.bus != true) {
+                if (month >= 4 && money >= 400) {
+                    money = money - 400;
+                    document.querySelector('.news').textContent = "SimPort, LLC just unlocked Buses! Onlookers look forward to higher income."
+                }
+               }
+               break;
+               case "advBus":
+               if (unlocked.advancedBus != true) {
+                if (month >= 6 && money >= 600) {
+                    money = money - 600;
+                    document.querySelector('.news').textContent = "SimPort, LLC just unlocked Advanced Buses! Onlookers look forward to higher income."
+                }
+               }
+               break;
+          default:
+               break;
+     }
+}
+//checking for requirements (unlocking)
+function updateUnlockValidity() {
+     if(month >= 2 && money >= 200){
+          unlockBoxes.advancedTaxi.style.color = "green";
+     }
+     else{
+          unlockBoxes.advancedTaxi.style.color = "red";
+     }
+     if(month >= 4 && money >= 400){
+          unlockBoxes.bus.style.color = "green";
+     }
+     else{
+          unlockBoxes.bus.style.color = "red";
+     }
+     if(month >= 6 && money >= 600){
+          unlockBoxes.advancedBus.style.color = "green";
+     }
+     else{
+          unlockBoxes.advancedBus.style.color = "red";
+     }
+}
+//asset acquirement
+function getAsset(asset){
+     switch (asset) {
+          case 'basicTaxi':
+               assets.taxis++;
+               money = money - 120;
+               break;
+          
+          default:
+               break;
+     }
+     document.querySelector('.money').textContent = money;
+}
+
+//income from vehicles
+//taxi
+function taxi(){
+  for(var i = 0; i < assets.taxis; i++){
+    money = money + (4 * Math.floor(Math.random() * (assets.taxiPaxCapacity + assets.taxiPopularity + 1)));
+  }
+  for(var v = 0; v < assets.advancedTaxis; v++){
+    money = money + (7 * Math.floor(Math.random() * (assets.advancedTaxiPaxCapacity + assets.taxiPopularity + 1)));
+  }
+}
+
+function bus(){
+  for(var i = 0; i < assets.buses; i++){
+    money = money + (3 * Math.floor(Math.random() * (assets.busPaxCapacity + assets.busPopularity + 1)));
+  }
+  for(var v = 0; v < assets.advancedBuses; v++){
+    money = money + (6 * Math.floor(Math.random() * (assets.advancedBusPaxCapacity + assets.busPopularity + 1)));
+  }
+}
+
+//on start
+updateUnlockValidity();
